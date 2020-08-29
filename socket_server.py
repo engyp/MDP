bf2fbf7620199fe8cbe4a3aba84e499832c5460c
engyp <ENGY0013@e.ntu.edu.sg>
2020-08-30 00:18:26 +0800

    a

--- socket_server.py
+++ socket_server.py
@@ -18,7 +18,7 @@ print 'Connected'
 
 # awaiting for message
 while True:
-	data = conn.recv(1024)
+	data = conn.recv(1024).decode("utf-8")
 	print 'I sent a message back in response to: ' + data
 	reply = ''
 
