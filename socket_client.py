b88b83b27931668365f0f34498e730fe2e52e992
engyp <ENGY0013@e.ntu.edu.sg>
2020-08-30 00:14:01 +0800

    a

--- socket_client.py
+++ socket_client.py
@@ -1,15 +1,15 @@
 import socket
 
-HOST = '' # Enter IP or Hostname of your server
+HOST = '192.168.30.1' # Enter IP or Hostname of your server
 PORT = 12345 # Pick an open Port (1000+ recommended), must match the server port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
 #Lets loop awaiting for your input
 while True:
-	command = raw_input('Enter your command: ')
-	s.send(command)
+	command = input('Enter your command: ')
+	s.send(command.encode('utf-8'))
 	reply = s.recv(1024)
 	if reply == 'terminate':
 		break
-	print reply
+	print(reply)
