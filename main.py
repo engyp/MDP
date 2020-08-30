b8297f90c894ce3c5d5d46dbd733a950b79528ce
engyp <ENGY0013@e.ntu.edu.sg>
2020-08-30 19:43:22 +0800

    a

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+btConnect = BluetoothConnection()
+
+try:
+	btConnect.connect()
+	while True:
+		data = self.client_sock.recv(1024)
+		if len(data) == 0: break
+		print("received [%s]" % data)
+	btConnect.disconnect()
+	
+	except IOError:
+		pass
