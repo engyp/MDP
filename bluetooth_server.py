f6d84b4c8a5b4784d3a40a12d2ee73b43914c78b
engyp <ENGY0013@e.ntu.edu.sg>
2020-08-30 19:00:09 +0800

    a

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -6,34 +6,46 @@
 
 from bluetooth import *
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",8)) # channel 8
-server_sock.listen(1)
 
-port = server_sock.getsockname()[1]
+def connect():
+	try:
+		server_sock=BluetoothSocket( RFCOMM )
+		server_sock.bind(("",8)) # channel 8
+		server_sock.listen(1)
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+		port = server_sock.getsockname()[1]
 
-advertise_service( server_sock, "MDPGrp30",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ])
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
+		uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+		advertise_service( server_sock, "MDPGrp30",
+		                   service_id = uuid,
+		                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+		                   profiles = [ SERIAL_PORT_PROFILE ])
+		                   
+		print("Waiting for connection on RFCOMM channel %d" % port)
+
+		client_sock, client_info = server_sock.accept()
+		print("Accepted connection from ", client_info)
+	
+	except Exception as e:
+        print("Bluetooth connection error")
+
+def disconnect():
+	try:
+		client_sock.close()
+		server_sock.close()
+		print("Bluetooth disconnected")
+
+	except Exception as e:
+        print("Bluetooth disconnection error")
+
 
 try:
+	connect()
     while True:
         data = client_sock.recv(1024)
         if len(data) == 0: break
         print("received [%s]" % data)
+    disconnect()
 except IOError:
     pass
-
-print("disconnected")
-
-client_sock.close()
-server_sock.close()
-print("all done")
