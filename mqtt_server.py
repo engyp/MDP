a399242545b2a518fa89cf14bfb14d6724812073
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-02 16:24:21 +0800

    a

--- mqtt_server.py
+++ mqtt_server.py
@@ -38,6 +38,7 @@ class MqttServer():
 
     def on_message_android(client, userdata, msg):
         message = str(msg.payload.decode("utf-8"))
+        print("oi")
         if message.split(",")[0] == "android":
             print("hi")
             self.pcConnect.send(message.split(",")[1])
