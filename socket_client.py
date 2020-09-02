a390556afc0c28082bd00097623a86b12c14ea49
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-02 18:55:37 +0800

    a

--- socket_client.py
+++ socket_client.py
@@ -6,12 +6,26 @@ PORT = 12345 # Pick an open Port (1000+ recommended), must match the server port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
-#Lets loop awaiting for your input
-while True:
-	#command = input('Enter your command: ')
-	#s.send(command.encode('utf-8'))
-	#publish.single("rpi/android", command.encode('utf-8'), hostname="192.168.30.1")
-	reply = s.recv(2048).decode("utf-8")
-	#if reply == 'terminate':
-	#	break
-	print(reply)
+def send():
+	#Lets loop awaiting for your input
+	while True:
+		command = input('Enter your text: ')
+		s.send(command.encode('utf-8'))
+		#publish.single("rpi/android", command.encode('utf-8'), hostname="192.168.30.1")
+
+
+def receive():
+	#Lets loop awaiting for your input
+	while True:
+		#command = input('Enter your command: ')
+		#s.send(command.encode('utf-8'))
+		#publish.single("rpi/android", command.encode('utf-8'), hostname="192.168.30.1")
+		reply = s.recv(2048).decode("utf-8")
+		#if reply == 'terminate':
+		#	break
+		print(reply)
+
+
+
+threading.Thread(target=send, name = 'send Thread').start()
+threading.Thread(target=receive, name = 'receive Thread').start()
