ea6b3ef06e28bfc7036e4e44d018973ece792471
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-03 23:16:43 +0800

    a

--- mqtt_server.py
+++ mqtt_server.py
@@ -21,19 +21,19 @@ class MqttServer():
     def on_message(self, client, userdata, msg):
         print(msg.topic+": "+str(msg.payload.decode("utf-8")))
 
-    def on_message_arduino(self, client, userdata, msg):
+    def on_msg_arduino(self, client, userdata, msg):
         message = str(msg.payload.decode("utf-8"))
         if message.split(",")[0] == "arduino":
             print("arduino" + message)
             self.sConnect.send(message.split(",")[1])
 
-    def on_message_android(self, client, userdata, msg):
+    def on_msg_android(self, client, userdata, msg):
         message = str(msg.payload.decode("utf-8"))
         if message.split(",")[0] == "android":
             print("android" + message)
             self.btConnect.send(message.split(",")[1])
 
-    def on_message_pc(self, client, userdata, msg):
+    def on_msg_pc(self, client, userdata, msg):
         message = str(msg.payload.decode("utf-8"))
         if message.split(",")[0] == "pc":
             print("pc"+ message)
@@ -43,16 +43,17 @@ class MqttServer():
         try:
             # Create an MQTT client and attach our routines to it.
             self.client = mqtt.Client()
-            
-            
-            
-            self.client.message_callback_add("rpi/arduino", self.on_message_arduino)
-            self.client.message_callback_add("rpi/android", self.on_message_android)
-            self.client.message_callback_add("rpi/pc", self.on_message_pc)
+            self.client.connect(self.hostname, 1883, 60)
             self.client.on_connect = self.on_connect
             self.client.on_message = self.on_message
-            self.client.connect(self.hostname, 1883, 60)
-            self.client.subscribe("rpi/#")
+            self.client.subscribe("rpi/arduino")
+            self.client.subscribe("rpi/android")
+            self.client.subscribe("rpi/pc")
+            self.client.message_callback_add("rpi/arduino", self.on_msg_arduino)
+            self.client.message_callback_add("rpi/android", self.on_msg_android)
+            self.client.message_callback_add("rpi/pc", self.on_msg_pc)
+             
+            
              
             # Process network traffic and dispatch callbacks. This will also handle
             # reconnecting. Check the documentation at
