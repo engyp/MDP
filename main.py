8402ec689de2e5a616bfe95be5df42e3eb734e83
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-04 15:28:01 +0800

    a

--- main.py
+++ main.py
@@ -5,89 +5,93 @@ import sys, traceback, threading
 import paho.mqtt.publish as publish
 import mqtt_server
 
+def __init__(self):
+        self.btConnect = None
+        self.pcConnect = None
+        self.sConnect = None
 
-def bluetooth_loop(mqttServer):
+def bluetooth_loop(mqttServer, self):
 	#while True:
 		try:
-			mqttServer.btConnect = BluetoothConnection()
-			mqttServer.btConnect.disconnect()
-			mqttServer.btConnect.connect()
+			self.mqttServer.btConnect = BluetoothConnection()
+			self.mqttServer.btConnect.disconnect()
+			self.mqttServer.btConnect.connect()
 			while True:
-				data = mqttServer.btConnect.receive()
+				data = self.mqttServer.btConnect.receive()
 				if data is None: break
 				print("received [%s] from android" % data)
 				#mqttServer.btConnect.send("\nreply back from rpi")
 				publish.single("android", data, hostname="192.168.30.1")
-			mqttServer.btConnect.disconnect()
+			self.mqttServer.btConnect.disconnect()
 
 		except KeyboardInterrupt:
-			mqttServer.btConnect.disconnect()
+			self.mqttServer.btConnect.disconnect()
 			
 		except Exception:
 			print("Main exec - Bluetooth connection error: ")
 			traceback.print_exc(limit=10, file=sys.stdout)
 
-def pc_loop(mqttServer):
+def pc_loop(mqttServer, self):
 	#while True:
 		try:
-			mqttServer.pcConnect = SocketConnection()
-			mqttServer.pcConnect.disconnect()
-			mqttServer.pcConnect.connect()
+			self.mqttServer.pcConnect = SocketConnection()
+			self.mqttServer.pcConnect.disconnect()
+			self.mqttServer.pcConnect.connect()
 			while True:
-				data = mqttServer.pcConnect.receive()
+				data = self.mqttServer.pcConnect.receive()
 				if data == 'quit': break
 				print("received [%s] from PC" % data)
 				publish.single("pc", data, hostname="192.168.30.1")
-			mqttServer.pcConnect.disconnect()
+			self.mqttServer.pcConnect.disconnect()
 
 		except KeyboardInterrupt:
-			mqttServer.pcConnect.disconnect()
+			self.mqttServer.pcConnect.disconnect()
 
 		except Exception:
 			print("Main exec - Socket connection error: ")
 			traceback.print_exc(limit=10, file=sys.stdout)
 
-def arduino_loop(mqttServer):
+def arduino_loop(mqttServer, self):
 	#while True:
 		try:
-			mqttServer.sConnect = SerialConnection()
-			mqttServer.sConnect.disconnect()
-			mqttServer.sConnect.connect()
+			self.mqttServer.sConnect = SerialConnection()
+			self.mqttServer.sConnect.disconnect()
+			self.mqttServer.sConnect.connect()
 			while True:
-				data = mqttServer.sConnect.receive()
+				data = self.mqttServer.sConnect.receive()
 				if data is None: break
 				print("received [%s] from arduino" % data) 
 				publish.single("arduino", data, hostname="192.168.30.1")
-			mqttServer.sConnect.disconnect()
+			self.mqttServer.sConnect.disconnect()
 
 		except KeyboardInterrupt:
-			mqttServer.sConnect.disconnect()
+			self.mqttServer.sConnect.disconnect()
 
 		except Exception:
 			print("Main exec - Serial connection error: ")
 			traceback.print_exc(limit=10, file=sys.stdout)
 
 try:
-	mqttServer = mqtt_server.MqttServer()
+	self.mqttServer = mqtt_server.MqttServer()
 
-	threading.Thread(target=bluetooth_loop, args=((mqttServer,)), name = 'Bluetooth Thread').start()
-	threading.Thread(target=pc_loop, args=((mqttServer,)), name = 'PC Thread').start()
-	threading.Thread(target=arduino_loop, args=((mqttServer,)), name = 'Arduino Thread').start()
+	#threading.Thread(target=bluetooth_loop, args=((mqttServer,)), name = 'Bluetooth Thread').start()
+	threading.Thread(target=pc_loop, args=((self.mqttServer,)), name = 'PC Thread').start()
+	#threading.Thread(target=arduino_loop, args=((mqttServer,)), name = 'Arduino Thread').start()
 
 	try:
-		mqttServer.run()
+		self.mqttServer.run()
 	except KeyboardInterrupt:
 		print("1111111111111111111111111111")
-		mqttServer.btConnect.disconnect()
-		mqttServer.pcConnect.disconnect()
-		mqttServer.sConnect.disconnect()
-		mqttServer.client.loop_stop()
-		mqttServer.client.disconnect()
+		self.mqttServer.btConnect.disconnect()
+		self.mqttServer.pcConnect.disconnect()
+		self.mqttServer.sConnect.disconnect()
+		self.mqttServer.client.loop_stop()
+		self.mqttServer.client.disconnect()
 
 except KeyboardInterrupt:
 	print("22222222222222222222222222")
-	mqttServer.btConnect.disconnect()
-	mqttServer.pcConnect.disconnect()
-	mqttServer.sConnect.disconnect()
-	mqttServer.client.loop_stop()
-	mqttServer.client.disconnect()
+	self.mqttServer.btConnect.disconnect()
+	self.mqttServer.pcConnect.disconnect()
+	self.mqttServer.sConnect.disconnect()
+	self.mqttServer.client.loop_stop()
+	self.mqttServer.client.disconnect()
