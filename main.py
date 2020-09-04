0b43bafb51596a6081d18c0dc692e9002823c56a
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-04 22:29:09 +0800

    a

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from serial_server import SerialConnection
 import sys, traceback, threading
 import paho.mqtt.publish as publish
 import mqtt_server
+import concurrent.futures
 
 
 def bluetooth_loop(mqttServer):
@@ -75,18 +76,20 @@ mqttServer = mqtt_server.MqttServer()
 #sThread.start()
 #sThread.join()
 
-btThread = threading.Thread(target=bluetooth_loop, args=((mqttServer,)), name = 'Bluetooth Thread')
-btThread.setDaemon(True)
-btThread.start()
+# btThread = threading.Thread(target=bluetooth_loop, args=((mqttServer,)), name = 'Bluetooth Thread')
+# btThread.setDaemon(True)
+# btThread.start()
 
-pcThread = threading.Thread(target=pc_loop, args=((mqttServer,)), name = 'PC Thread')
-pcThread.setDaemon(True)
-pcThread.start()
-
-
-btThread.join()
-pcThread.join()
+# pcThread = threading.Thread(target=pc_loop, args=((mqttServer,)), name = 'PC Thread')
+# pcThread.setDaemon(True)
+# pcThread.start()
 
 
+# btThread.join()
+# pcThread.join()
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+	executor.submit(bluetooth_loop, mqttServer)
+	executor.submit(pc_loop, mqttServer)
 
 mqttServer.run()
