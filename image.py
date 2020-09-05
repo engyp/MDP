3c3b3e66aac9c1191a3b92726f3d7967746045a8
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-06 01:29:52 +0800

    Update image.py

--- image.py
+++ image.py
@@ -26,20 +26,15 @@ def findDes(images):
 
 def findID(img, desList, threshold=10):
 	kp2,des2 = orb.detectAndCompute(img,None)
-	# FLANN parameters
-	FLANN_INDEX_KDTREE = 1
-	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-	search_params = dict(checks=50)   # or pass empty dictionary
-	
-	flann = cv.FlannBasedMatcher(index_params,search_params)
+	bf = cv2.BFMatcher()
 	matchList = []
 	finalVal = -1
 	try:
 		for des in desList:
-			matches = flann.knnMatch(des,des2,k=2)
+			matches = bf.knnMatch(des,des2,k=2)
 			good = []
 			for m,n in matches:
-				if m.distance < 0.7*n.distance:
+				if m.distance < 0.90*n.distance:
 					good.append([m])
 			matchList.append(len(good))
 	except:
