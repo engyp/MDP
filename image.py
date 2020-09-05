79ac6370cfe56e6233c35d37edbd982dde5c2f64
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-05 21:24:25 +0800

    a

--- image.py
+++ image.py
@@ -1,9 +1,21 @@
+import sys
+sys.path.append("..")
 import cv2
 import numpy as numpy
 
-img1 = cv2.imread('ImagesQuery/up.JPG')
-img2 = cv2.imread('ImagesTrain/upTrain.JPG')
+img1 = cv2.imread('ImagesQuery/up.JPG',0)
+img2 = cv2.imread('ImagesTrain/upTrain.JPG',0)
 
+orb = cv2.ORB_create()
+
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+imgKp1 = cv2.drawKeypoints(img1,kp1,None)
+imgKp2 = cv2.drawKeypoints(img2,kp2,None)
+
+cv2.imshow('Kp1',imgKp1)
+cv2.imshow('Kp2',imgKp2)
 cv2.imshow('img1',img1)
 cv2.imshow('img2',img2)
 cv2.waitKey(0)
