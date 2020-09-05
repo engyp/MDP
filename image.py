caa483b2403e7a211b856e5988f983cff3ecdb89
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-06 00:16:46 +0800

    a

--- image.py
+++ image.py
@@ -1,18 +1,42 @@
-import sys
+import sys, cv2, os
 sys.path.append("..")
-import cv2
 import numpy as numpy
 
-img1 = cv2.imread('ImagesQuery/up.JPG',0)
-img2 = cv2.imread('ImagesTrain/upTrain.JPG',0)
-
+path = ('ImagesQuery')
 orb = cv2.ORB_create(nfeatures=1000)
+############ Import images
+images = []
+classNames = []
+myList = os.listdir(path)
+print('Total Classes Detected', len(myList))
+for cl in myList:
+	imgCur = cv2.imread(f'{path}/{cl}',0)
+	images.append(imgCur)
+	classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
 
-# imgKp1 = cv2.drawKeypoints(img1,kp1,None)
-# imgKp2 = cv2.drawKeypoints(img2,kp2,None)
+def findDes(images):
+	desList = []
+	for img in images:
+		kp,des = orb.detectAndCompute(img,None)
+		desList.append(des)
+	return desList
+
+desList = findDes(images)
+print(len(desList))
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+	success,img2 = cap.read()
+	imgOriginal = img2.copy()
+	img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+	cv2.imshow('img2',imgOriginal)
+	cv2.waitKey(1)
 
 # imgKp1 = img1.copy()
 # for marker in kp1:
@@ -22,19 +46,19 @@ kp2, des2 = orb.detectAndCompute(img2,None)
 # for marker in kp2:
 # 	imgKp2 = cv2.drawMarker(imgKp2, tuple(int(i) for i in marker.pt), color=(0, 255, 0))
 
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1,des2,k=2)
 
-good = []
-for m,n in matches:
-	if m.distance < 0.75*n.distance:
-		good.append([m])
-print(len(good))
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+# good = []
+# for m,n in matches:
+# 	if m.distance < 0.75*n.distance:
+# 		good.append([m])
+# print(len(good))
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 
-# cv2.imshow('Kp1',imgKp1)
-# cv2.imshow('Kp2',imgKp2)
-cv2.imshow('img1',img1)
-cv2.imshow('img2',img2)
-cv2.imshow('img3',img3)
-cv2.waitKey(0)
+# # cv2.imshow('Kp1',imgKp1)
+# # cv2.imshow('Kp2',imgKp2)
+# cv2.imshow('img1',img1)
+# cv2.imshow('img2',img2)
+# cv2.imshow('img3',img3)
+# cv2.waitKey(0)
