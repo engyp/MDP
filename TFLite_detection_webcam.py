1d3021f168f656775b2b7064ccab2def500ddbec
engyp <ENGY0013@e.ntu.edu.sg>
2020-09-07 00:04:22 +0800

    Update TFLite_detection_webcam.py

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -30,7 +30,7 @@ class VideoStream:
     def __init__(self,resolution=(640,480),framerate=30):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
-        self.out = None
+        self.out = cv2.VideoWriter('hi.h264', cv2.VideoWriter_fourcc(*'H264'), 25, (640,480))
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
@@ -62,6 +62,9 @@ class VideoStream:
     def read(self):
 	# Return the most recent frame
         return self.frame
+
+    def writeFrame(self):
+        self.out.write(self.frame)
 
     def stop(self):
 	# Indicate that the camera and thread should be stopped
@@ -156,50 +159,8 @@ input_std = 127.5
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-
-frames_per_second = 24.0
-res = '720p'
-
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
-
-# Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
-    "480p": (640, 480),
-    "720p": (1280, 720),
-    "1080p": (1920, 1080),
-    "4k": (3840, 2160),
-}
-
-
-# grab resolution dimensions and set video capture to it.
-def get_dims(cap, res='1080p'):
-    width, height = STD_DIMENSIONS["480p"]
-    if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
-    ## change the current caputre device
-    ## to the resulting resolution
-    change_res(cap, width, height)
-    return width, height
-
-VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-}
-
-def get_video_type(filename):
-    filename, ext = os.path.splitext(filename)
-    if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
-    return VIDEO_TYPE['avi']
-
 # Initialize video stream
 videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
-out = cv2.VideoWriter('hi.h264', get_video_type('hi.h264'), 25, get_dims(self.stream, '720p'))
 time.sleep(1)
 
 #for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
@@ -210,8 +171,7 @@ while True:
 
     # Grab frame from video stream
     frame1 = videostream.read()
-    out.write(frame1)
-
+    videostream.writeFrame()
     # Acquire frame and resize to expected shape [1xHxWx3]
     frame = frame1.copy()
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
